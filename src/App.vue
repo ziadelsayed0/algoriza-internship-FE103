5539801f075a63e57774bbf965f4bbddfd3502b6
Ziad Elsayed <106037654+ziadelsayed0@users.noreply.github.com>
2023-12-02 15:45:43 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navbar
+      v-if="
+        this.$route.name !== 'login' &&
+        this.$route.name !== 'register'
+      "
+    />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  name: "App",
+  components: { Navbar },
+};
+</script>
+<style></style>
