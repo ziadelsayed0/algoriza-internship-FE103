e5b69e187661264d04ad5e94a54d9c3ac192fbfd
Ziad Elsayed <106037654+ziadelsayed0@users.noreply.github.com>
2023-12-02 15:49:10 +0300

    Delete src directory

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "@/assets/styles/main.css";
-import { createPinia } from "pinia";
-
-createApp(App).use(createPinia()).use(router).mount("#app");
